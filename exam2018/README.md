b8169c8d6966daf12c88f6ae09e05cf0d783fb25
Romain ODET <romain.odet@lecole-ldlc.com>
2018-12-05 13:38:17 +0100

    initial commit

---
+++ exam2018/README.md
@@ -0,0 +1,99 @@
+# LE JEU
+
+Ce projet est un mini jeu de PACMAN en mode texte.
+PACMAN se déplace dans un niveau 2D, son but étant de manger toutes les pac-gommes sans se faire manger.
+Quand PACMAN a mangé toutes les pac-gomme le joueur a gagné.
+
+Le niveau est composé des éléments suivants :
+
+- 'C' Le joueur (PACMAN !)
+- 'X' Ennemi(s)
+- '#' mur (PACMAN et les ennemis ne peuvent pas passer a travers !)
+- '.' pac-gomme (miam !)
+- 'o' super-gomme, PACMAN devient invincible et il peut manger les ennemis
+
+Le joueur utilise les touches suivantes pour se déplacer :
+
+- 'L' ou 'l' pour aller à gauche
+- 'R' ou 'r' pour aller à droite
+- 'U' ou 'u' pour aller en haut
+- 'D' ou 'D' pour aller en bas
+
+Au départ, PACMAN se trouve à la position (1,1).
+
+La position de PACMAN est stockée dans une liste à 2 éléments représentant l'abscisse et l'ordonnée.
+L'origine est situé en haut à gauche, les coordonnées sont donc toujours positives.
+
+Le jeu se termine 
+ - quand PACMAN rencontre un ennemi => le joueur perd
+ - quand PACMAN a mangé toutes les pac-gommes => le joueur gagne
+
+Le jeu est en rafraîchissement lent, c'est à dire qu'après chaque déplacement de PACMAN, le programme affiche
+le niveau avec tous les personnages.
+
+Je vous fourni un code de base (presque) fonctionnel, à vous de jouer maintenant !
+
+# VOTRE TRAVAIL
+
+Créer un projet dans PyCharm, avec ce README et le code de base en python.
+**Vous devrez uniquement modifier ces 2 fichiers.**
+
+Vous allez devoir implémenter un maximum de fonctionnalités.
+Quand vous avez terminé une fonctionnalité (c'est à dire que vous l'avez **testée** !),
+marquez là dans ce fichier README avec deux tildes (~) en début de ligne et deux tildes en fin (sans espace) :
+~~- E0. Lire le code~~
+
+En fin d'examen, déposez sur moodle une archive zip portant votre nom, avec votre programme python et votre fichier README.md.
+Vous pouvez proposer une fonctionnalité par niveau de difficulté.
+
+Pour valider le module, vous devez amasser au minimum **18 points**.
+Avant de commencer les fonctionnalités modérées et avancés, vous **devez** implémenter toutes les fonctionnalités faciles.
+
+## Fonctionnalités facile (2 pt)
+
+- E1. Afficher un message en **vert** au début de votre programme.
+- E2. Changer le niveau (ne mettez pas trop de gommes pour que vos tests ne soient pas trop longs !).
+- E3. Demander son age au joueur, si il a moins de 12 ans quitter, sinon continuer.
+- E4. L'utilisateur peut quitter le jeu en tapant 'q' ou 'Q' comme déplacement.
+- E5. Vérifier, dans la fonction 'get_case_content' que la position donnée ne sort pas du niveau, si cela sort, retourner `None`.
+- E6. Compléter la fonction `move_pacman` de sorte que PACMAN se déplace vraiment.
+  * INDICE => Cette fonction est très similaire à la fonction  `remove_gum_from_map`
+- E7. Afficher le nombre de pac-gomme mangées en fin de jeu.
+- E8. Implémenter la fin "PACMAN gagne" 
+  * INDICE => Compter le nombre de '.' dans le niveau, si PACMAN a mangé ce nombre de pac-gomme il a gagné !
+- E9. Quand PACMAN mange une super-gomme, il devient invincible et il peut manger les ennemis.
+  * INDICE => C'est très similaire à quand PACMAN mange les pac-gommes !
+- E10. Afficher le nombre d'ennemi mangés en fin de partie.
+- E11. _votre fonctionnalité ici_
+
+## Fonctionnalités modérés (4 pt)
+
+- M1. Quand PACMAN mange une super-gomme il devient bleu et les ennemis deviennent rose
+- M2. Gérer un nouveau type de case `B`, quand PACMAN passe dessus, il peut manger les murs (et donc passer à travers !)
+  * INDICE => Attention cela ne concerne que PACMAN, pas les ennemis
+- M3. Après chaque déplacement de PACMAN, l'ennemi se déplace aléatoirement (sans passer a travers les murs!)
+  * INDICES => L'ennemi se déplace dans une des directions principales (haut, bas, gauche ou droite).
+  * INDICES => Dans un premier temps, l'ennemi va manger aussi les gommes, super-gommes et autre bonus, tant pis !
+  * INDICES => L'ennemi peut, bien entendu, manger PACMAN et alors le joueur perd !
+  * INDICES => Attention à ne plus faire bouger l'ennemi quand PACMAN l'a mangé !
+- M4. _votre fonctionnalité ici_
+
+## Fonctionnalités avancées (6 pt)
+
+- A1. _valable uniquement si M3 fait_ Gérer le déplacement des ennemis sans qu'ils mangent les gommes, super-gommes et autres bonus 
+  * INDICE => Vous pouvez par exemple utiliser 2 représentations du niveau, une sans les personnages mobiles et une avec.
+- A2. _valable uniquement si M3 et A1 fait_ Gérer plusieurs ennemis (paramétrable)
+  * INDICE => la position de l'ennemi va devenir une liste de positions des différents ennemis
+- A3. Le(s) ennemi(s) se rapproche de PACMAN quand il(s) se déplace(nt) (laisser la fonction de M3 en commentaire dans votre code)
+  * INDICE => Pour chaque ennemi prenez la direction qui minimize la distance avec PACMAN !
+- A4. _votre fonctionnalité ici_
+
+# CONSEILS
+
+- Lisez d'abord le code de base en entier pour bien le comprendre
+- Prenez en particulier le temps de lire les commentaires !
+- Si vous avez trop d'erreur, n'hésitez pas à retélécharger le code original et recommencer !
+- Attention le niveau n'est pas stocké de la meme manière que dans le projet labyrinthe !
+- Je vous conseil (sans vous l'imposer) d'implémenter les fonctionnalités dans l'ordre, meme si il y a peu de dépendances
+- Prenez soin de l'interface utilisateur (messages, gestion des erreurs, etc.)
+- Si vous voulez afficher des messages de debug, utiliser la fonction `debug_text`
---
+++ exam2018/pacman.py
@@ -0,0 +1,175 @@
+###
+### Author : Antoine Scherrer <antoine.scherrer@lecole-ldlc.com>
+### License : GPL
+###
+
+# The game map, as a large string
+# Be careful not to include useless spaces on the right when you modify the map !
+game_map = """
+########
+.C ..o #
+#.## # #
+#.##.# #
+#. . .X#
+########
+"""
+
+# Definition of each component of the map
+PACMAN = 'C'
+ENNEMY = 'X'
+WALL = '#'
+SUPERGUM = 'o'
+GUM = '.'
+EMPTY = ' '
+
+# Compute width and height of the map
+width = len(game_map.split('\n')[1])
+# we need to subtract 2 because of the newline at beginning and the newline at the end of the string
+height = len(game_map.split('\n')) - 2
+
+
+# Generate a red colored text
+def red_text(txt):
+    return '\033[31;1m' + txt + '\033[0m'
+
+
+# Generate a green colored text
+def green_text(txt):
+    return '\033[32;1m' + txt + '\033[0m'
+
+
+# Generate a blue colored text
+def blue_text(txt):
+    return '\033[34;1m' + txt + '\033[0m'
+
+
+# Generate a pink colored text
+def pink_text(txt):
+    return '\033[35;1m' + txt + '\033[0m'
+
+
+# Generate a debug text. Use this to show debug messages,
+# you can consider the player won't see these messages
+def debug_text(txt):
+    return '\033[37m' + '[DEBUG]' + txt + '\033[0m'
+
+
+# get the index of a position in the game_map string
+def get_map_index(position):
+    # We need to compute the index of that char in the string.
+    # We need to add 1 to the width because the the "new line" characters
+    return 1 + position[0] + (width + 1) * position[1]
+
+
+# return the character in the game_map at given coordinates
+def get_case_content(position):
+    # TODO check if position goes outside of the map, return None in that case
+    return game_map[get_map_index(position)]
+
+
+# remove a gum of the map
+def remove_gum_from_map(position):
+    # use this line to modify the game_map global variable in the function
+    global game_map
+    # just in case, check that the case at given position contains a gum !
+    if get_case_content(position) != GUM:
+        print(debug_text('ERROR: trying to remove a non-existing gum'))
+        return
+
+    # convert the map into a list (so that we can change a character !)
+    game_map_list = list(game_map)
+    # remove the gum (put the empty char at the position of the gum)
+    game_map_list[get_map_index(position)] = EMPTY
+    # convert the list back to a string, that will be the updated game map
+    game_map = "".join(game_map_list)
+
+
+# move pacman at new position in the map
+def move_pacman(current_position, next_position):
+    # use this line to modify the game_map global variable in the function
+    global game_map
+    # TODO : complete this function (you can get inspired by remove_gum_from_map)
+    # You will need the two parameters of the function
+    # current_position is PACMAN's current position in the map
+    # next_position is PACMAN's next position (after move)
+    print(debug_text('we are now moving PACMAN'))
+
+
+# display the map, with fancy colors !
+def show_map(map):
+    # for each char of the map
+    for char in map:
+        if char == WALL:
+            print(char, end='')
+        elif char == ENNEMY:
+            print(red_text(char), end='')
+        elif char == PACMAN:
+            print(green_text(char), end='')
+        elif char == GUM:
+            print(pink_text(char), end='')
+        else:
+            print(char, end='')
+
+
+# Program starts here !
+if __name__ == "__main__":
+
+    # Inital positions of PACMAN and ennemy
+    pacman_position = [1, 1]
+    enemy_position = [6, 4]
+
+    while True:
+        # Display the game map (this is what "slow refresh game' implies)
+        show_map(game_map)
+
+        # Ask the user where he wants to go
+        # convert user input to uppercase
+        move = input('Votre déplacement ?').upper()
+
+        # We copy pacman_position in next_position
+        next_position = list(pacman_position)
+        # Update next_position
+        if move == 'L':
+            next_position[0] -= 1
+        elif move == 'R':
+            next_position[0] += 1
+        elif move == 'U':
+            next_position[1] -= 1
+        elif move == 'D':
+            next_position[1] += 1
+        else:
+            print('Move not understood, try again.')
+            continue
+
+        # Depending of the content of the case, move PACMAN and take required actions
+        case = get_case_content(next_position)
+        if case == WALL:
+            print(red_text('Vous enterred a wall, try again'))
+        elif case == ENNEMY:
+            print(red_text('ENNEMY THERE => YOU DIE // GAME OVER'))
+            break
+        elif case == GUM:
+            print(green_text('Yummy !'))
+            remove_gum_from_map(next_position)
+            # update PACMAN position
+            move_pacman(pacman_position, next_position)
+            pacman_position = list(next_position)
+        elif case == SUPERGUM:
+            # TODO Deal with SUPERGUM effect
+            print(pink_text('You are now invincible'))
+            # update PACMAN position
+            move_pacman(pacman_position, next_position)
+            pacman_position = list(next_position)
+        elif case == EMPTY:
+            print(pink_text('Nothing here, keep moving'))
+            # update PACMAN position
+            move_pacman(pacman_position, next_position)
+            pacman_position = list(next_position)
+        elif case == None:
+            print(red_text('You went outside of the map, stupid !'))
+        else:
+            print(debug_text('Something went wrong !!'))
+
+        # TODO Check is game is finished, in that case display some messages
+
+        # TODO Make the ennemy move
